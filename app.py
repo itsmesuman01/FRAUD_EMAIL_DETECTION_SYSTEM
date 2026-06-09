4481d3de80ee050f26c6a46433704691ff84f7bb
itsmesuman01 <sumanpandey977n@gmail.com>
2026-06-09 21:38:50 +0545

    Debug Mode Removed

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 import joblib
+import os
 
 app = Flask(__name__)
 
@@ -23,4 +24,5 @@ def home():
     return render_template("index.htm", result=result)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
